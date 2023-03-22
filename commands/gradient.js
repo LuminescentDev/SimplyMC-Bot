58a7190c564139f494cc2c72c4b806bea10499d9
AkiraDev <akira.devjs@gmail.com>
2023-03-22 08:46:05 -0700

    fix for new api

--- commands/gradient.js
+++ commands/gradient.js
@@ -1,49 +1,77 @@
 const Discord = require("discord.js");
-const fetch = (...args) => import('node-fetch').then(({ default: e }) => e(...args));
+const fetch = (...args) => import("node-fetch").then(({ default: e }) => e(...args));
 module.exports = {
-	name: "gradient",
-	category: "test",
-	description: "pp!",
-	options: require("../options/gradient.js"),
-	ephemeral: false,
+  name: "gradient",
+  category: "test",
+  description: "pp!",
+  options: require("../options/gradient.js"),
+  ephemeral: false,
 
-    async autoComplete(client, interaction) {
-        const focusedValue = interaction.options.getFocused();
-        const choices = [
-            { name: '&#rrggbb', value: '&#$1$2$3$4$5$6$f$c' },
-			{ name: "<#rrggbb>", value: "<#$1$2$3$4$5$6>$f$c" },
-			{ name: "&x&r&r&g&g&b&b", value: "&x&$1&$2&$3&$4&$5&$6$f$c" },
-			{ name: "§x§r§r§g§g§b§b", value: "§x§$1§$2§$3§$4§$5§$6$f$c" },
-        ];
-        const filtered = choices.filter(choice => choice.name.toLowerCase().startsWith(focusedValue.toLowerCase()));
-        if (!filtered.length) filtered.push({ name: focusedValue, value: focusedValue });
-        interaction.respond(filtered);
-    },
+  async autoComplete(client, interaction) {
+    const focusedValue = interaction.options.getFocused();
+    const choices = [
+      { name: "&#rrggbb", value: "&#$1$2$3$4$5$6$f$c" },
+      { name: "<#rrggbb>", value: "<#$1$2$3$4$5$6>$f$c" },
+      { name: "&x&r&r&g&g&b&b", value: "&x&$1&$2&$3&$4&$5&$6$f$c" },
+      { name: "§x§r§r§g§g§b§b", value: "§x§$1§$2§$3§$4§$5§$6$f$c" },
+    ];
+    const filtered = choices.filter((choice) =>
+      choice.name.toLowerCase().startsWith(focusedValue.toLowerCase())
+    );
+    if (!filtered.length)
+      filtered.push({ name: focusedValue, value: focusedValue });
+    interaction.respond(filtered);
+  },
 
-	async execute(client, interaction, args) {
-		const text = args.getString("text");
-		const colors = args.getString("colors").split(",");
-		const format = args.getString("format") ? args.getString("format") : "&#$1$2$3$4$5$6$f$c";
-		const formatChar = args.getString("formatChar") ? args.getString("formatChar") : "&";
-		const bold = args.getBoolean("bold");
-		const italic = args.getBoolean("italic");
-		const underline = args.getBoolean("underline");
-		const strikethrough = args.getBoolean("strikethrough");
-		const formatting = compress([bold, italic, underline, strikethrough]);
-		const gradient = await fetch(`https://api.simplymc.art/gradient?text=${encodeURIComponent(text)}&colors=${encodeURIComponent(colors.join(","))}&format=${encodeURIComponent(format)}&formatChar=${encodeURIComponent(formatChar)}&formatting=${formatting}`)
-		.then(res => res.json())
-		const previewbuffer = Buffer.from(gradient.Preview.split(",")[1], 'base64');
-		const preview = new Discord.AttachmentBuilder().setName("preview.png").setFile(previewbuffer);
-		interaction.editReply({ content: gradient.Output, files: [preview] });
-	},
+  async execute(client, interaction, args) {
+    try {
+      const text = args.getString("text");
+      const colors = args.getString("colors").split(",");
+      const prefix = args.getString("prefix") ? args.getString("prefix") : "";
+      const format = args.getString("format")
+        ? args.getString("format")
+        : "&#$1$2$3$4$5$6$f$c";
+      const formatChar = args.getString("formatChar")
+        ? args.getString("formatChar")
+        : "&";
+      const bold = args.getBoolean("bold");
+      const italic = args.getBoolean("italic");
+      const underline = args.getBoolean("underline");
+      const strikethrough = args.getBoolean("strikethrough");
+
+      const gradient = await fetch(
+        `https://api.simplymc.art/gradient?text=${encodeURIComponent(
+          text
+        )}&colors=${encodeURIComponent(
+          colors.join(",")
+        )}&prefix=${encodeURIComponent(
+          prefix + " "
+        )}&format=${encodeURIComponent(
+          format
+        )}&formatchar=${encodeURIComponent(
+          formatChar
+        )}&bold=${encodeURIComponent(
+          bold
+        )}&italic=${encodeURIComponent(
+          italic
+        )}&underline=${encodeURIComponent(
+          underline
+        )}&strikethrough=${encodeURIComponent(
+          strikethrough
+        )}`
+      ).then((res) => res.json());
+    //   const previewbuffer = Buffer.from(
+    //     gradient.Preview.split(",")[1],
+    //     "base64"
+    //   );
+    //   const preview = new Discord.AttachmentBuilder()
+    //     .setName("preview.png")
+    //     .setFile(previewbuffer);
+      interaction.editReply({ content: gradient.output });
+    } catch (error) {
+      interaction.editReply(
+        `An error occurred while executing the command: ${error.message}`
+      );
+    }
+  },
 };
-
-// Takes an array of boolean values and turns them into a number
-function compress(values) {
-	let output = 0;
-	for (let i = 0; i < values.length; i++) {
-		const value = values[i];
-		output |= ~~value << i;
-	}
-	return output;
-  }
--- options/gradient.js
+++ options/gradient.js
@@ -1,53 +1,63 @@
-const { SlashCommandStringOption, SlashCommandBooleanOption } = require('discord.js');
+const {
+  SlashCommandStringOption,
+  SlashCommandBooleanOption,
+} = require("discord.js");
 
-module.exports = async function(cmd){
-    cmd.addStringOption(
-        new SlashCommandStringOption()
-            .setName('text')
-            .setDescription('The text to use')
-            .setRequired(true)
+module.exports = async function (cmd) {
+  cmd
+    .addStringOption(
+      new SlashCommandStringOption()
+        .setName("text")
+        .setDescription("The text to use")
+        .setRequired(true)
     )
     .addStringOption(
-        new SlashCommandStringOption()
-        .setName('colors')
-        .setDescription('The colors to use')
+      new SlashCommandStringOption()
+        .setName("colors")
+        .setDescription("The colors to use")
         .setRequired(true)
     )
     .addStringOption(
         new SlashCommandStringOption()
-        .setName('format')
-        .setDescription('the gradient format')
+        .setName("prefix")
+        .setDescription("Usually used for commands")
+        .setRequired(false)
+    )
+    .addStringOption(
+      new SlashCommandStringOption()
+        .setName("format")
+        .setDescription("the gradient format")
         .setRequired(false)
         .setAutocomplete(true)
     )
     .addStringOption(
-        new SlashCommandStringOption()
-        .setName('formatchar')
-        .setDescription('the gradient format character')
+      new SlashCommandStringOption()
+        .setName("formatchar")
+        .setDescription("the gradient format character")
         .setRequired(false)
     )
     .addBooleanOption(
-        new SlashCommandBooleanOption()
-        .setName('bold')
-        .setDescription('make the text bold')
+      new SlashCommandBooleanOption()
+        .setName("bold")
+        .setDescription("make the text bold")
         .setRequired(false)
     )
     .addBooleanOption(
-        new SlashCommandBooleanOption()
-        .setName('italic')
-        .setDescription('make the text italic')
+      new SlashCommandBooleanOption()
+        .setName("italic")
+        .setDescription("make the text italic")
         .setRequired(false)
     )
     .addBooleanOption(
-        new SlashCommandBooleanOption()
-        .setName('underline')
-        .setDescription('make the text underline')
+      new SlashCommandBooleanOption()
+        .setName("underline")
+        .setDescription("make the text underline")
         .setRequired(false)
     )
     .addBooleanOption(
-        new SlashCommandBooleanOption()
-        .setName('strikethrough')
-        .setDescription('make the text strikethrough')
+      new SlashCommandBooleanOption()
+        .setName("strikethrough")
+        .setDescription("make the text strikethrough")
         .setRequired(false)
-    )
-}
+    );
+};
